aca103d4444c6bc041281084101c5e2d4b5b2664
TANKING <king@likeyunba.com>
2019-04-24 12:34:47 +0800

    一些相关的配置
    一些相关的配置

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$mchid = '1511820741'; // PAYJS 商户号
+$key   = '10T2aVOG89FdIlMG'; // 通信密钥
+$localhost = 'rm-bp1ue7qo15k5oy0go.mysql.rds.aliyuncs.com'; //数据库地址
+$dbname = 'rke0u01huw'; //数据库名
+$dbuser = 'rke0u01huw'; //数据库帐号
+$dbpwd = 'Likeyun2016'; //数据库密码
+$tbname = "wechatpay";  //表名
+?>
