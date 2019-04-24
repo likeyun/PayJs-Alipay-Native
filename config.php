5ecc8390f10e439a56d7d59ea9d99ba76fac4395
TANKING <king@likeyunba.com>
2019-04-24 12:34:28 +0800

    一些相关的配置
    一些相关的配置

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mchid = 'xxx'; // PAYJS 商户号
-$key   = 'xxx'; // 通信密钥
-$localhost = 'xxx'; //数据库地址
-$dbname = 'xxx'; //数据库名
-$dbuser = 'xxx'; //数据库帐号
-$dbpwd = 'xxx'; //数据库密码
-$tbname = "xxx";  //表名
-?>
